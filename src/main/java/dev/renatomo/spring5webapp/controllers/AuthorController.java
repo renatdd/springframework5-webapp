d3b1910f7ea39b48691897439c5504d7e6a2dfdf
Renato Feitosa <pythaqua@gmail.com>
2022-01-08 22:06:25 -0300

    feat: Add Thymeleaf views to authors and books

---
+++ src/main/java/dev/renatomo/spring5webapp/controllers/AuthorController.java
@@ -0,0 +1,24 @@
+package dev.renatomo.spring5webapp.controllers;
+
+import dev.renatomo.spring5webapp.repositories.AuthorRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class AuthorController {
+
+    private final AuthorRepository authorRepository;
+
+    public AuthorController(AuthorRepository authorRepository) {
+        this.authorRepository = authorRepository;
+    }
+
+    @RequestMapping("/authors")
+    public String getAuthors(Model model) {
+
+        model.addAttribute("authors", authorRepository.findAll());
+
+        return "authors/list";
+    }
+}
--- src/main/java/dev/renatomo/spring5webapp/controllers/BookController.java
+++ src/main/java/dev/renatomo/spring5webapp/controllers/BookController.java
@@ -19,7 +19,7 @@ public class BookController {
 
         model.addAttribute("books", bookRepository.findAll());
 
-        return "books";
+        return "books/list";
     }
 }
 
